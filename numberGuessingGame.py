1109b4864fd06a1c4b1b1f7f6dce74c71e7a89a0
Zach Ruiz <powersbooth10@gmail.com>
2023-07-23 18:52:25 -0500

    added opening print statemnets and logic for choice of difficulty.

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -1,3 +1,31 @@
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer. 
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player. 
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
+
 from art import logo
+import random
+target_number = random.randint(1, 100)
+guesses = 0
 
 print(logo)
+print("Welcome to the Number Guessing Game!")
+print("I'm thinking of a number between 1 and 100")
+print(f"Pssst, the correct answer is {target_number}")
+
+difficulty_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+if difficulty_level == "easy":
+  guesses = 10
+elif difficulty_level == "hard":
+  guesses = 5
+else:
+  print("Invalid input")
+
+while guesses != 0:
+  print(f"You have {guesses} attempts remaining to guess the number.")
