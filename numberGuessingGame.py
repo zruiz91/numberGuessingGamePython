c54494bdc2c7dd4ac56e9a3c9caa647927040134
Zach Ruiz <powersbooth10@gmail.com>
2023-07-23 19:19:16 -0500

    added a loop that prompts the user for a guess and calculates if its lower or higher. Basic functionality complete. Still need to refactor.

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -8,10 +8,22 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
+
+# def choose_difficulty():
+#   difficulty_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+
+#   if difficulty_level == "easy":
+#     guesses = 10
+#   elif difficulty_level == "hard":
+#     guesses = 5
+#   else:
+#     print("Invalid input")
+
 from art import logo
 import random
 target_number = random.randint(1, 100)
 guesses = 0
+game_over = False
 
 print(logo)
 print("Welcome to the Number Guessing Game!")
@@ -27,5 +39,25 @@ elif difficulty_level == "hard":
 else:
   print("Invalid input")
 
-while guesses != 0:
-  print(f"You have {guesses} attempts remaining to guess the number.")
+while game_over == False:
+  if guesses != 0:
+    print(f"You have {guesses} attempts remaining to guess the number.")
+    new_guess = int(input("Make a guess: "))
+
+    if new_guess > target_number:
+      guesses -= 1
+      print("Too High")
+      print("Guess Again")
+    elif new_guess < target_number:
+      guesses -= 1
+      print("Too Low")
+      print("Guess Again")
+    else:
+      print(f"You got it! The answer was {target_number}.")
+      game_over = True
+
+  else:
+    print("You've run out of guesses, you lose.")
+    game_over = True
+
+    
