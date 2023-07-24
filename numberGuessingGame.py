160011b96b3ce293a66bb3d011e92e6a48aedcb4
Zach Ruiz <powersbooth10@gmail.com>
2023-07-24 14:28:22 -0500

    final refactor. refactored the comparison lofgic into its own function.

--- numberGuessingGame.py
+++ numberGuessingGame.py
@@ -8,74 +8,59 @@
 # If they run out of turns, provide feedback to the player. 
 # Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
 
-
-# def choose_difficulty():
-#   difficulty_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-
-#   if difficulty_level == "easy":
-#     guesses = 10
-#   elif difficulty_level == "hard":
-#     guesses = 5
-#   else:
-#     print("Invalid input")
-# def decide_difficulty(difficulty_level):
-#   if difficulty_level == "easy":
-#     return guesses + 10
-#   elif difficulty_level == "hard":
-#     return guesses + 5
-#   else:
-#     print("Invalid input")
-#     decide_difficulty(difficulty_level)
-    
   
 from art import logo
 import random
-target_number = random.randint(1, 100)
-game_over = False
-guesses = 0
 
-def decide_difficulty(difficulty_level):
+
+
+def decide_difficulty():
+  difficulty_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
   if difficulty_level == "easy":
-    return guesses + 10
+    return 10
   elif difficulty_level == "hard":
-    return guesses + 5
+    return 5
   else:
     print("Invalid input")
     decide_difficulty(difficulty_level)
 
-
-  
-
-print(logo)
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100")
-print(f"Pssst, the correct answer is {target_number}")
-
-difficulty_level = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-
-guesses = decide_difficulty(difficulty_level)
-
-
-
-while game_over == False:
-  if guesses != 0:
-    print(f"You have {guesses} attempts remaining to guess the number.")
-    new_guess = int(input("Make a guess: "))
+def check_guess(new_guess, target_number, guesses):
 
     if new_guess > target_number:
-      guesses -= 1
       print("Too High")
-      print("Guess Again")
+      return guesses - 1
     elif new_guess < target_number:
-      guesses -= 1
       print("Too Low")
-      print("Guess Again")
+      return guesses - 1
     else:
       print(f"You got it! The answer was {target_number}.")
-      game_over = True
 
-  else:
-    print("You've run out of guesses, you lose.")
-    game_over = True
 
-    
+  
+def game():
+  print(logo)
+  print("Welcome to the Number Guessing Game!")
+  print("I'm thinking of a number between 1 and 100")
+  target_number = random.randint(1, 100)
+  print(f"Pssst, the correct answer is {target_number}")
+  
+  
+  
+  guesses = decide_difficulty()
+  
+  new_guess = 0
+  
+  
+  while new_guess != target_number:
+      print(f"You have {guesses} attempts remaining to guess the number.")
+      new_guess = int(input("Make a guess: "))
+
+      guesses = check_guess(new_guess, target_number, guesses)
+
+      if guesses == 0:
+        print("You've run out of guesses, you lose.")
+        return
+      elif new_guess != target_number:
+        print("Guess Again")
+      
+game()
